5a21523dd933cd908320e832341d58cd90ccb124
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2024-03-01 14:06:24 +0100

    added personalContact

---
+++ src/common/Contact/personalContact.js
@@ -0,0 +1,3 @@
+export const email = "dorotazielinska1@protonmail.com";
+export const gitHub = "https://github.com/DorotaZielinska";
+export const linkedin = "https://www.linkedin.com/in/dorota-zielinska-584b60293/";
