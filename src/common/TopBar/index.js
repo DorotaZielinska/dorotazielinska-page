dcf29b2cd29583d6024d740555dff83348972f55
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2024-03-02 21:04:51 +0100

    cosmetic change

--- src/common/TopBar/index.js
+++ src/common/TopBar/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Burger } from "./Burger/burger";
-import { Menu } from "./Menu/menu";
 import { Footer } from "../../common/Footer/footer";
+import { Menu } from "./Menu/menu";
+import { Burger } from "./Burger/burger";
 
 export const TopBar = () => {
   const [open, setOpen] = useState(false);
