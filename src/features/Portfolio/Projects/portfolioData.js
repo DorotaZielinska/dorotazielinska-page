4c56635ad13d4c41e600c657b65dd97c97f86c40
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2024-03-02 21:04:28 +0100

    added portfolioData

---
+++ src/features/Portfolio/Projects/portfolioData.js
@@ -0,0 +1,30 @@
+import image1 from "../../../common/Assets/moviebrovser.screen.png";
+import image2 from "../../../common/Assets/portfolio.screen.png";
+import image3 from "../../../common/Assets/currencyconverter.screen.png";
+
+export const PortfolioData = [
+  {
+   id: 1,
+    title: "Movie Browser",
+    code: "https://github.com/DorotaZielinska/movies-browser",
+    demo: "https://www.movies-browser.nl/popular/movies&language=en-US&page=1",
+    image: [image1],
+    alt: "Movie Browser screenshot",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    code: "https://github.com/DorotaZielinska/dorota-zielinska-homepage",
+    demo: "https://dorotazielinska.github.io/dorota-zielinska-homepage/",
+    image: [image2],
+    alt: "Portfolio screenshot",
+  },
+  {
+    id: 3,
+    title: "Currency Converter",
+    code: "https://github.com/DorotaZielinska/currencies-converter-react",
+    demo: "https://dorotazielinska.github.io/currencies-converter-react/",
+    image: [image3],
+    alt: "Currency Converter screenshot",
+  },
+];
