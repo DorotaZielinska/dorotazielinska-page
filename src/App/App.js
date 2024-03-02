b45a9ecb092708df034718988e3245382a5b67a0
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2024-03-02 21:08:03 +0100

    added Browser Router  and route to home, portfolio i contact

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,23 @@
-import { Clock } from "../common/Clock/Clock";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom/cjs/react-router-dom";
+import { TopBar } from "../common/TopBar";
+import { Contact } from "../features/Contact/contact";
+import { Portfolio } from "../features/Portfolio/portfolio";
 
 function App() {
   return (
     <>
-      <Clock />
+      <BrowserRouter>
+        <TopBar />
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
